a98f749db056d78ffe8f9b0f85540f69603075aa
OjijuanHaman <ojijuan.haman@bryanuniversity.edu>
2021-08-14 13:34:11 -0500

    added week1 assignments

---
+++ week1/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <>
+   <Greeting />
+   <Greeting />
+   <Greeting />
+   </>
+  );
+}
+
+export default App;
---
+++ week1/my-app/src/Greeting.js
@@ -0,0 +1,10 @@
+function Greeting (){
+
+    const greeting = "Good Morning"
+    return (
+        <h1>{greeting}</h1>
+        
+    );
+}
+
+export default Greeting;
